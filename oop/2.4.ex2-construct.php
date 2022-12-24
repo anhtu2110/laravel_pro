1644396d0873db87818d53d50d0a07b404e9da37
Dao Anh Tu <daotu2110@gmail.com>
2022-12-24 19:52:44 +0700

    github/laravel_pro

--- oop/2.4.ex2-construct.php
+++ oop/2.4.ex2-construct.php
@@ -8,4 +8,16 @@ class Rectangle
     {
         echo "Đã tạo đối tượng";
     }
+    public function getPerimeter()
+    {
+        return 2 * ($this->width + $this->height);
+    }
+    public function getArea()
+    {
+        return $this->width * $this->height;
+    }
 }
+$a = new Rectangle;
+$a->width = 20;
+$a->height = 30;
+echo $a->getPerimeter();
