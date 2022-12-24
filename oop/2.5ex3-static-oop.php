f8893849a1fc58ae5905343dc8c5512a0f0eecd3
Dao Anh Tu <daotu2110@gmail.com>
2022-12-24 20:12:59 +0700

    github/laravel_pro

---
+++ oop/2.5ex3-static-oop.php
@@ -0,0 +1,30 @@
+<?php
+
+use Demo as GlobalDemo;
+
+class Rectangle
+{
+    public static $width;
+    public static $height;
+
+    public static function getPerimeter()
+    {
+        return 2 * (self::$width + self::$height);
+    }
+
+    public static function getArea()
+    {
+        return self::$width * self::$height;
+    }
+}
+
+class Demo
+{
+    function __construct()
+    {
+        Rectangle::$width = 10;
+        Rectangle::$height = 20;
+        echo Rectangle::getPerimeter();
+    }
+}
+new Demo;
